73c01996dcd66eb569828cbb9da679471644cf59
condini-mastheus <matheus.condini@gmail.com>
2022-04-03 17:14:31 -0300

    install colyseus and setup a simple example

--- src/client/scripts/scenes/mainScene.ts
+++ src/client/scripts/scenes/mainScene.ts
@@ -1,14 +1,21 @@
 import PhaserLogo from '../objects/phaserLogo'
 import FpsText from '../objects/fpsText'
 
+import { Client, Room } from 'colyseus.js'
+
 export default class MainScene extends Phaser.Scene {
   fpsText
 
+  private client: Client
+  private room?: Room
+
   constructor() {
     super({ key: 'MainScene' })
+
+    this.client = new Client('ws://localhost:2567')
   }
 
-  create() {
+  async create() {
     new PhaserLogo(this, this.cameras.main.width / 2, 0)
     this.fpsText = new FpsText(this)
 
@@ -19,6 +26,16 @@ export default class MainScene extends Phaser.Scene {
         fontSize: '24px'
       })
       .setOrigin(1, 0)
+
+    await this.join()
+  }
+
+  async join() {
+    this.room = await this.client.joinOrCreate('lobby')
+
+    this.room.onMessage('message', message => {
+      console.log(message)
+    })
   }
 
   update() {
